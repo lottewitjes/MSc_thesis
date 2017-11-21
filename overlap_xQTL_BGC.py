de1d958bc20a695455bb501f0761c3a3b8f3a5ad
witje010 <lottewitjes@outlook.com>
2017-11-21 14:48:58 +0100

    make BGC_parser loop over all BGC files in dir

--- overlap_xQTL_BGC.py
+++ overlap_xQTL_BGC.py
@@ -1,10 +1,10 @@
 #!/usr/bin/evn/python
 """A Python script that overlaps xQTL with BGCs found with plantiSMASH.
 
-python overlap_xQTL_BGC.py <BGC_file> <eQTL_file> <mQTL_file>
+python overlap_xQTL_BGC.py <BGC_dir> <eQTL_file> <mQTL_file>
 
 Keyword arguments:
-    BGC_file --> A file containing the BGCs
+    BGC_dir --> A directory containing x_BGC.txt output files from plantiSMASH
     eQTL_file --> A file containing the eQTLs
     mQTL_file --> A file containing the mQTLs
 
@@ -32,27 +32,32 @@ def xQTL_parser(xQTL_file):
         thelist.append(elements)
     return thelist
 
-def BGC_parser(BGC_file):
-    thefile = open(BGC_file, "r")
+def BGC_parser(BGC_dir):
+    filelist = os.listdir(BGC_dir)
+    filelist = [file for file in filelist if "BGC" in file]
     thedic = {}
-    next(thefile) #skip the header
-    for line in thefile:
-        elements = line.split("\t")
-        chr, cluster_id = elements[0].split("_c")
-        type = elements[1]
-        from_bp, to_bp = elements[3].split(";")
-        genes = elements[4].split(";")
-        genes = [re.sub(r"-.*", "", gene) for gene in genes]
-        thedic[cluster_id] = [type, chr, from_bp, to_bp, genes]
+    for file in filelist:
+        file_path = "{}/{}".format(BGC_dir, file)
+        thefile = open(file_path, "r")
+        next(thefile) #skip the header
+        for line in thefile:
+            elements = line.split("\t")
+            chr, cluster_id = elements[0].split("_c")
+            type = elements[1]
+            from_bp, to_bp = elements[3].split(";")
+            genes = elements[4].split(";")
+            genes = [re.sub(r"-.*", "", gene) for gene in genes]
+            thedic[cluster_id] = [type, chr, from_bp, to_bp, genes]
     return thedic
 
 if __name__ == "__main__":
     #Get files from command line
-    BGC_file = argv[1]
+    BGC_dir = argv[1]
     eQTL_file = argv[2]
     mQTL_file = argv[3]
 
     #Parse the files
-    BGC_dic = BGC_parser(BGC_file)
-    eQTL_list = xQTL_parser(eQTL_file)
-    mQTL_list = xQTL_parser(mQTL_file)
+    BGC_dic = BGC_parser(BGC_dir)
+    print BGC_dic
+    #eQTL_list = xQTL_parser(eQTL_file)
+    #mQTL_list = xQTL_parser(mQTL_file)
