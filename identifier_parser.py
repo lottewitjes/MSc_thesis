50f1f9ca236e76b636a12778d478cbe698de3066
witje010 <lottewitjes@outlook.com>
2017-11-22 10:14:25 +0100

    added more documentation

--- identifier_parser.py
+++ identifier_parser.py
@@ -22,6 +22,13 @@ __email__ = "lottewitjes@outlook.com"
 
 
 def parser_ids(id1_id2_file):
+    """A function to parse a file with IDs into a dictionary with one ID as the key and the other as the value.
+
+    Keyword arguments:
+        id1_id2_file - a file containing 2 columns with matching IDs
+    Returns:
+        dic - a dictionary with one ID as the key and the matching ID as value
+    """
     thefile = open(id1_id2_file, "r")
     dic = {}
     for line in thefile:
@@ -31,6 +38,13 @@ def parser_ids(id1_id2_file):
     return dic
 
 def parser_file_to_be_changed(file_to_be_changed):
+    """A function to parse a file which IDs have to be changed into a list of list containing the elements per  row.
+
+    Keyword arguments:
+        file_to_be_changed - a file which IDs have to be changed
+    Returns:
+        thelist - a list of lists containing the values in the file_to_be_changed as a list per row.
+    """
     thefile = open(file_to_be_changed, "r")
     thelist = []
     for line in thefile:
@@ -39,6 +53,15 @@ def parser_file_to_be_changed(file_to_be_changed):
     return thelist
 
 def write_changed_file(list_to_be_changed, id1_id2_dic, output_name):
+    """A function to write a new file with values from list_to_be_changed but with altered IDs according to id1_id2_dic.
+
+    Keyword arguments:
+        list_to_be_changed - a list of lists containing the values in the file_to_be_changed as a list per row.
+        id1_id2_dic - a dictionary with one ID as the key and the matching ID as value.
+        output_name - the name of the result file.
+    Returns:
+        output_name - a file, like file_to_be_changed, with changed IDs.
+    """
     thefile = open(output_name, "w")
     for alist in list_to_be_changed:
         if alist[0] in id1_id2_dic:
@@ -49,11 +72,14 @@ def write_changed_file(list_to_be_changed, id1_id2_dic, output_name):
     thefile.close()
 
 if __name__ == "__main__":
+    #Get files from command line and format the name of the output file
     file_to_be_changed = argv[1]
     id1_id2_file = argv[2]
     output_name = "{}_parsed".format(file_to_be_changed)
 
+    #Parse the files into dictionary and list of lists
     id1_id2_dic = parser_ids(id1_id2_file)
     list_to_be_changed = parser_file_to_be_changed(file_to_be_changed)
 
+    #Change the IDs of the file_to_be_changed and write the new file
     write_changed_file(list_to_be_changed, id1_id2_dic, output_name)
