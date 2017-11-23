9f95f911a2ccdc25411218df080a59ef628257b9
witje010 <lottewitjes@outlook.com>
2017-11-23 11:50:17 +0100

    changed name

--- gene_identifier_parser.py
+++ gene_identifier_parser.py
@@ -30,13 +30,13 @@ def parser_ids(id1_id2_file):
     Returns:
         dic - a dictionary with one ID as the key and the matching ID as value
     """
-    thefile = open(id1_id2_file, "r")
-    dic = {}
-    for line in thefile:
-        elements = line.split()
-        if len(elements) != 1:
-            dic[elements[1]] = elements[0]
-    return dic
+    with open(id1_id2_file, "r") as thefile:
+        dic = {}
+        for line in thefile:
+            elements = line.split()
+            if len(elements) != 1:
+                dic[elements[1]] = elements[0]
+        return dic
 
 def parser_file_to_be_changed(file_to_be_changed):
     """A function to parse a file which IDs have to be changed into a list of list containing the elements per  row.
@@ -46,12 +46,12 @@ def parser_file_to_be_changed(file_to_be_changed):
     Returns:
         thelist - a list of lists containing the values in the file_to_be_changed as a list per row.
     """
-    thefile = open(file_to_be_changed, "r")
-    thelist = []
-    for line in thefile:
-        elements = line.split()
-        thelist.append(elements)
-    return thelist
+    with open(file_to_be_changed, "r") as thefile:
+        thelist = []
+        for line in thefile:
+            elements = line.split()
+            thelist.append(elements)
+        return thelist
 
 def write_changed_file(list_to_be_changed, id1_id2_dic, output_name):
     """A function to write a new file with values from list_to_be_changed but with altered IDs according to id1_id2_dic.
@@ -63,14 +63,13 @@ def write_changed_file(list_to_be_changed, id1_id2_dic, output_name):
     Returns:
         output_name - a file, like file_to_be_changed, with changed IDs.
     """
-    thefile = open(output_name, "w")
-    for alist in list_to_be_changed:
-        if alist[0] in id1_id2_dic:
-            line = "{}  {}  {}  {}  {}  {}".format(id1_id2_dic[alist[0]], alist[1], alist[2], alist[3], alist[4], alist[5])
-        else:
-            line = "{}  {}  {}  {}  {}  {}".format(alist[0], alist[1], alist[2], alist[3], alist[4], alist[5])
-        thefile.write(line + "\n")
-    thefile.close()
+    with open(output_name, "w") as thefile:
+        for alist in list_to_be_changed:
+            if alist[0] in id1_id2_dic:
+                line = "{}  {}  {}  {}  {}  {}".format(id1_id2_dic[alist[0]], alist[1], alist[2], alist[3], alist[4], alist[5])
+            else:
+                line = "{}  {}  {}  {}  {}  {}".format(alist[0], alist[1], alist[2], alist[3], alist[4], alist[5])
+            thefile.write(line + "\n")
 
 if __name__ == "__main__":
     #Get files from command line and format the name of the output file
